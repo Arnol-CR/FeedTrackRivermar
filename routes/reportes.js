203089cc723ece557481dd1f4cc48191a5a51755
Arnol-CR <arnolcarranza24@gmail.com>
2026-08-07 15:31:37 -0600

    Agrega columna de Equipos encendidos desde VW_HorasTrabajas_Bitacora

--- routes/reportes.js
+++ routes/reportes.js
@@ -79,7 +79,7 @@ router.get('/racion', async (req, res) => {
           WHERE FechaConsumo = @Fecha
             AND HorasTrabajadas > 0
             AND IdUbicacionLagSector IN (${idsEstanques.join(',')})
-            AND IdTipoEquipos = 1
+            AND IdTipoEquipo = 1
           GROUP BY IdUbicacionLagSector
         `);
       const mapaLecturas = {};
